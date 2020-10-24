cac582ca6f8a61f801bb8d3f8537978e85142d36
Wanda <wmmcinturff@gmail.com>
2020-10-24 15:58:53 -0500

    re-styled bootstrap cards and font-awesome button imgs; fixed wonky rendering

--- src/javascripts/components/cards/boardCard.js
+++ src/javascripts/components/cards/boardCard.js
@@ -1,20 +1,17 @@
 // import boardData from '../../helpers/data/boardData';
 
 const boardMaker = (boardObject) => {
-  const domString = `<div class="card boardCards${boardObject.boardUid} mb-3" style="width: 18rem;" >
-  </div>
-     <img class="card-img-top" src="${boardObject.image}" alt="Card image cap">
-     <div class="card-body">
-     <h5 class="card-title">${boardObject.name}</h5>
-     <div class= "button-group">
-     <button class="btn btn-info see-board-pins" id="${boardObject.boardUid}">Pins</button>
-     <button class="btn btn-danger remove-board" id="${boardObject.boardUid}">Remove Board</button>
-      </div>  
-     </div> 
-     </div>      
-    </div>`;
-
+  const domString = `<div class="card boardCards" style="width: 18rem;" id="${boardObject.boardUid}">
+    <img src="${boardObject.image}" class="card-img-top" alt="${boardObject.name}" />
+    <div class="card-body">
+      <h5 class="card-title">${boardObject.name}</h5>
+      <p class="card-description">${boardObject.description}</p>
+       <a href="#" id="${boardObject.boardUid}" class="btn btn-info view-pin"><i class="far fa-eye"></i>View Pins</a>
+       <a href="#" id="${boardObject.boardUid}" class="btn btn-primary delete-board"><i class="fas fa-minus-circle"></i>Delete Board</a>
+    </div>
+  </div>`;
   return domString;
 };
+// NEED TO DELETE BOARD LATER //
 
 export default { boardMaker };
--- src/javascripts/components/cards/pinCards.js
+++ src/javascripts/components/cards/pinCards.js
@@ -1,7 +1,7 @@
 // import pinData from '../../helpers/data/pinData';
 
 const buildPinCard = (pinObject) => {
-  const domString = `<div class="card dinn" style="width: 18rem;" id="${pinObject.uid}">
+  const domString = `<div class="card-pin" style="width: 18rem;" id="${pinObject.uid}">
     <img src="${pinObject.image}" class="card-img-top" alt="${pinObject.name}" />
     <div class="card-body">
       <h5 class="card-title">${pinObject.name}</h5>
